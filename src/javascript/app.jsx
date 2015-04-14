3c4c962e8e006d95ecb65b6e93eb627833633d44
Tommy Marshall <tommy.marshall@viget.com>
2015-04-14 10:49:17 -0400

    Adds quac

--- src/javascript/app.jsx
+++ src/javascript/app.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 
+var find = React.findDOMNode
+
+const BURRITO_COST = 7.25
+const QUAC_COST    = 1.95
+
 class App extends React.Component {
   constructor(props) {
     super(props)
 
     this.state = {
-      count : 0
+      count : 0,
+      quac  : false
     }
   }
 
@@ -14,14 +20,18 @@ class App extends React.Component {
       <main className="app-container">
         <h1>How Many Burritos</h1>
         <input ref="count" type="number" onChange={this.change.bind(this)} />
-        <h2>{this.state.count} flippin burritos</h2>
+        <input ref="quac" type="checkbox" onChange={this.change.bind(this)} />
+        <h2>{this.state.count} flippin burritos!</h2>
       </main>
     )
   }
 
   change() {
+    let money = parseInt(find(this.refs.count).value)
+    let quac = parseInt(find(this.refs.quac).checked ? QUAC_COST : 0)
+
     this.setState({
-      count : Math.round(React.findDOMNode(this.refs.count).value / 7)
+      count : money / (BURRITO_COST + quac)
     })
   }
 }
